be1ccac0853e3cd2020940b675b4c6ab5bfdded6
KrishnaVakte <86585840+KrishnaVakte@users.noreply.github.com>
2023-10-27 23:10:08 +0530

    verce.json added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import notes from './routes/notes.js'
 import cors from 'cors'
 
 const app=express()
-const port = 5500
+const port = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json())
